7eb7f6c961396058b30990c93833cc7ec69c68b4
ibrahim-abdirashid <neeykaraamo@gmail.com>
2025-11-13 15:03:02 +0300

    working header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,27 @@
+import { Link } from "react-router"
 
 
 
 const Header = () => {
   return (
-    <header>
+    <header className="bg-white shadow">
         {/* div-ka guud ee isku haya */}
-        <div>
+        <div className="max-w-7xl mx-auto  ">
             {/* div-ka right iyo left isku haya */}
+            <div className="flex justify-between h-16 items-center">
+
+                {/* left */}
+                <div className="flex">
+                    {/* logo */}
+                    <div className="shrink-0 flex items-center">
+                        <Link to="/" className="text-2xl font-bold text-orange-700">Blogify</Link>
+                    </div>
+                    {/* navigation */}
+                </div>
+
+                {/* right */}
+
+            </div>
         </div>
     </header>
   )
